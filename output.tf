665a55c3c8f411030d4230f1f6e97a0d1d813419
rickymaw <rickym@aplaceformom.com>
2020-05-28 15:35:43 -0700

    Removing unnecessary terraform outputs

--- output.tf
+++ output.tf
@@ -7,13 +7,3 @@ output "cloudfront_domain_name" {
   description = "The domain name corresponding to the distribution"
   value = aws_cloudfront_distribution.distribution.domain_name
 }
-
-output "bucket_website_endpoint" {
-  description = "The domain name corresponding to the distribution"
-  value = data.aws_s3_bucket.selected.0.website_endpoint
-}
-
-output "bucket_website_websitedomain" {
-  description = "The domain name corresponding to the distribution"
-  value = data.aws_s3_bucket.selected.0.website_domain
-}
